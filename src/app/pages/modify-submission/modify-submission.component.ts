e39332f36b853eab6f888f032fd831d811af0aab
SElmahi <elmahi79sanae@gmail.com>
2023-06-22 05:09:09 +0100

    kemelt

--- src/app/pages/modify-submission/modify-submission.component.ts
+++ src/app/pages/modify-submission/modify-submission.component.ts
@@ -3,14 +3,21 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { SubmitService } from '../../services/submit.service';
 import { Author, UserRole } from '../../models/Author';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, Validators, FormGroup, AbstractControl, ValidationErrors, FormArray } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  AbstractControl,
+  ValidationErrors,
+  FormArray,
+} from '@angular/forms';
 import { Submission } from 'src/app/models/Submission';
 import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-modify-submission',
   templateUrl: './modify-submission.component.html',
-  styleUrls: ['./modify-submission.component.css']
+  styleUrls: ['./modify-submission.component.css'],
 })
 export class ModifySubmissionComponent implements OnInit {
   submitForm: FormGroup;
@@ -19,39 +26,45 @@ export class ModifySubmissionComponent implements OnInit {
   coAuthors: Author[] = [];
   submissionId: string;
   submission: Submission;
-   loading = false;
+  loading = false;
   error: string;
 
-  constructor(private formBuilder: FormBuilder, private submitService: SubmitService, private snackBar: MatSnackBar, private router: Router, private route: ActivatedRoute) {
-    this.submitForm = this.formBuilder.group({
-      title: ['', Validators.required], // Add this line
-      abstractText: ['', Validators.required],
-      keywords: ['', Validators.required],
-      paper: ['', Validators.required],
-      submissionType: ['', Validators.required],
-      coAuthors: this.formBuilder.array([]),
-    }, { validators: this.fileSelectedValidator.bind(this) });
+  constructor(
+    private formBuilder: FormBuilder,
+    private submitService: SubmitService,
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.submitForm = this.formBuilder.group(
+      {
+        title: ['', Validators.required], // Add this line
+        abstractText: ['', Validators.required],
+        keywords: ['', Validators.required],
+        paper: ['', Validators.required],
+        submissionType: ['', Validators.required],
+        coAuthors: this.formBuilder.array([]),
+      },
+      { validators: this.fileSelectedValidator.bind(this) }
+    );
   }
-  
-  
 
   ngOnInit(): void {
     this.submissionId = this.route.snapshot.paramMap.get('id');
     this.getSubmissionData(this.submissionId);
-    
   }
-  
 
   getSubmissionData(id: string): void {
-    this.submitService.getSubmissionById(id).subscribe((submission: Submission) => {
-      this.submission = submission;
-      const { paper, ...submissionData } = submission;
-      this.submitForm.patchValue(submissionData);
-      console.log('Submission data:', submissionData);
-      
-    });
+    this.submitService
+      .getSubmissionById(id)
+      .subscribe((submission: Submission) => {
+        this.submission = submission;
+        const { paper, ...submissionData } = submission;
+        this.submitForm.patchValue(submissionData);
+        console.log('Submission data:', submissionData);
+      });
   }
- 
+
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
     this.submitForm.updateValueAndValidity(); // Trigger form validation update
@@ -63,7 +76,8 @@ export class ModifySubmissionComponent implements OnInit {
     }
 
     // Check for the file size limit
-    if (this.selectedFile.size > 5 * 1024 * 1024) { // 5MB in bytes
+    if (this.selectedFile.size > 5 * 1024 * 1024) {
+      // 5MB in bytes
       return { maxSize: true };
     }
 
@@ -73,26 +87,32 @@ export class ModifySubmissionComponent implements OnInit {
     if (this.submitForm.invalid) {
       return;
     }
-    const formData = { ...this.submitForm.value, customId: this.submission.customId };
+    const formData = {
+      ...this.submitForm.value,
+      customId: this.submission.customId,
+    };
     console.log('Form  modifed dd data:', formData); // Add this line
     this.updateSubmission(formData, this.selectedFile);
-  }  
+  }
 
   updateSubmission(formData: any, paper: File): void {
     this.loading = true;
     this.error = null;
-  
+
     // Update the submission object with the form values and the selected file
     const updatedSubmission = { ...this.submission, ...formData };
     if (paper) {
       updatedSubmission.paper = paper;
     }
-  
+
     // Remove the 'paper' property when sending the request
-  
-  
+
     this.submitService
-      .updateSubmission(this.submission.id, updatedSubmission, this.selectedFile)
+      .updateSubmission(
+        this.submission.id,
+        updatedSubmission,
+        this.selectedFile
+      )
       .subscribe(
         () => {
           this.loading = false;
@@ -105,9 +125,7 @@ export class ModifySubmissionComponent implements OnInit {
       );
   }
 
-  
-  
-getCoAuthorsControls(): AbstractControl[] {
-  return (this.submitForm.get('coAuthors') as FormArray).controls;
-}
+  getCoAuthorsControls(): AbstractControl[] {
+    return (this.submitForm.get('coAuthors') as FormArray).controls;
+  }
 }
--- src/app/pages/submit/submit.component.ts
+++ src/app/pages/submit/submit.component.ts
@@ -1,37 +1,70 @@
-
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SubmitService } from '../../services/submit.service';
 import { Author, UserRole } from '../../models/Author';
 import { Router } from '@angular/router';
-import { FormBuilder, Validators, FormGroup, AbstractControl, ValidationErrors, FormArray } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  AbstractControl,
+  ValidationErrors,
+  FormArray,
+} from '@angular/forms';
 @Component({
   selector: 'app-submit',
   templateUrl: './submit.component.html',
-  styleUrls: ['./submit.component.css']
+  styleUrls: ['./submit.component.css'],
 })
 export class SubmitComponent implements OnInit {
   submitForm: FormGroup;
   selectedFile: File;
   isSubmitting: boolean = false;
- 
 
-  constructor(private formBuilder: FormBuilder, private submitService: SubmitService, private snackBar: MatSnackBar, private router: Router) {
-    this.submitForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.required, this.confirmPasswordValidator.bind(this)]],
-      firstName: ['', [Validators.required, this.noSpecialCharsValidator.bind(this)]],
-      lastName: ['', [Validators.required, this.noSpecialCharsValidator.bind(this)]],
-      affiliation: ['', [Validators.required, this.noOnlyNumbersValidator.bind(this)]],
-      title: ['', Validators.required],
-      abstractText: ['', Validators.required],
-      keywords: ['', Validators.required],
-      paper: ['', Validators.required],
-      fileSize: [null] ,
-      submissionType: ['', Validators.required],
-     
-    }, { validators: this.fileSelectedValidator.bind(this) });
+  constructor(
+    private formBuilder: FormBuilder,
+    private submitService: SubmitService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    this.submitForm = this.formBuilder.group(
+      {
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.email,
+            Validators.pattern(
+              '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'
+            ),
+          ],
+        ],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: [
+          '',
+          [Validators.required, this.confirmPasswordValidator.bind(this)],
+        ],
+        firstName: [
+          '',
+          [Validators.required, this.noSpecialCharsValidator.bind(this)],
+        ],
+        lastName: [
+          '',
+          [Validators.required, this.noSpecialCharsValidator.bind(this)],
+        ],
+        affiliation: [
+          '',
+          [Validators.required, this.noOnlyNumbersValidator.bind(this)],
+        ],
+        title: ['', Validators.required],
+        abstractText: ['', Validators.required],
+        keywords: ['', Validators.required],
+        paper: ['', Validators.required],
+        fileSize: [null],
+        submissionType: ['', Validators.required],
+      },
+      { validators: this.fileSelectedValidator.bind(this) }
+    );
   }
 
   ngOnInit(): void {}
@@ -39,20 +72,20 @@ export class SubmitComponent implements OnInit {
     const file = this.selectedFile;
     if (!file) {
       return { noFileSelected: true };
-    } 
-  
+    }
+
     if (file.type !== 'application/pdf') {
       return { invalidFileType: true };
     }
-  
-    if (file.size > 5 * 1024 * 1024) { // 5MB in bytes
+
+    if (file.size > 5 * 1024 * 1024) {
+      // 5MB in bytes
       return { maxSize: true };
     }
-  
+
     return null;
   }
-  
-  
+
   noSpecialCharsValidator(control: AbstractControl) {
     if (control && (control.value !== null || control.value !== undefined)) {
       const regex = new RegExp('^[a-zA-Z ]*$');
@@ -78,81 +111,94 @@ export class SubmitComponent implements OnInit {
   }
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
-    this.submitForm.get('fileSize').setValue(this.selectedFile ? this.selectedFile.size : null);
+    this.submitForm
+      .get('fileSize')
+      .setValue(this.selectedFile ? this.selectedFile.size : null);
     this.submitForm.get('fileSize').updateValueAndValidity();
   }
-  
 
   onSubmit(): void {
     if (this.submitForm.invalid) {
       return;
     }
-  
-    this.isSubmitting = true;
-    this.submitService.submitPaper(this.submitForm.value, this.selectedFile).subscribe(response => {
-     
-      console.log('Submission successful');
-      this.isSubmitting = false;
 
-      // Show success snackbar
-      this.snackBar.open('Successfully  Submitted! Please check your email for confirmation.', 'Close', {
-        duration: 15000, // 10 seconds
-        horizontalPosition: 'center',
-        verticalPosition: 'top',
-        panelClass: ['snackbar-success', 'custom-snackbar', 'mat-snack-bar-container-center'] // Add your custom classes here
-      });
-      // Redirect to the confirmation view or clear the form
-      this.submitForm.reset();
-     
-    }, error => {
-      this.isSubmitting = false;
-      let errorMessage = 'An error occurred while submitting the paper. Please try again.';
-  
-      if (error.status) {
-        switch (error.status) {
-          case 409: // Email is already registered
-            errorMessage = 'Email is already registered.';
-            break;
-          case 413: // File size limit exceeded
-            errorMessage = 'File size must not exceed 5 MB.';
-            break;
-          case 400: // Invalid email format
-            errorMessage = 'Invalid email format.';
-            break;
-          case 500: // Internal server error
-            errorMessage = 'An unexpected error occurred on the server. Please try again later.';
-            break;
+    this.isSubmitting = true;
+    this.submitService
+      .submitPaper(this.submitForm.value, this.selectedFile)
+      .subscribe(
+        (response) => {
+          console.log('Submission successful');
+          this.isSubmitting = false;
+
+          // Show success snackbar
+          this.snackBar.open(
+            'Successfully  Submitted! Please check your email for confirmation.',
+            'Close',
+            {
+              duration: 15000, // 10 seconds
+              horizontalPosition: 'center',
+              verticalPosition: 'top',
+              panelClass: [
+                'snackbar-success',
+                'custom-snackbar',
+                'mat-snack-bar-container-center',
+              ], // Add your custom classes here
+            }
+          );
+          // Redirect to the confirmation view or clear the form
+          this.submitForm.reset();
+        },
+        (error) => {
+          this.isSubmitting = false;
+          let errorMessage =
+            'An error occurred while submitting the paper. Please try again.';
+
+          if (error.status) {
+            switch (error.status) {
+              case 409: // Email is already registered
+                errorMessage = 'Email is already registered.';
+                break;
+              case 413: // File size limit exceeded
+                errorMessage = 'File size must not exceed 5 MB.';
+                break;
+              case 400: // Invalid email format
+                errorMessage = 'Invalid email format.';
+                break;
+              case 500: // Internal server error
+                errorMessage =
+                  'An unexpected error occurred on the server. Please try again later.';
+                break;
+            }
+          }
+
+          this.snackBar.open(errorMessage, 'Close', {
+            duration: 10000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+            panelClass: [
+              'snackbar-error',
+              'custom-snackbar',
+              'mat-snack-bar-container-center',
+            ],
+          });
+        }
+      );
+  }
+
+  confirmPasswordValidator(control: AbstractControl) {
+    if (control && (control.value !== null || control.value !== undefined)) {
+      const confirmPassword = control.value;
+
+      const passwordControl = control.root.get('password');
+      if (passwordControl) {
+        const password = passwordControl.value;
+        if (confirmPassword !== password) {
+          return {
+            isError: true,
+          };
         }
       }
-  
-      this.snackBar.open(errorMessage, 'Close', {
-        duration: 10000,
-        horizontalPosition: 'center',
-        verticalPosition: 'top',
-        panelClass: ['snackbar-error', 'custom-snackbar', 'mat-snack-bar-container-center']
-      });
-    });
-  }
-  
-
- 
-
-
-confirmPasswordValidator(control: AbstractControl) {
-  if (control && (control.value !== null || control.value !== undefined)) {
-    const confirmPassword = control.value;
-
-    const passwordControl = control.root.get('password');
-    if (passwordControl) {
-      const password = passwordControl.value;
-      if (confirmPassword !== password) {
-        return {
-          isError: true,
-        };
-      }
     }
+    return null;
   }
-  return null;
-}
-
 }
